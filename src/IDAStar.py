00115534b83a068455720c29da6b084268782f11
okryzhan <sashakryzh@gmail.com>
2020-02-11 18:46:59 +0200

    IDA*

--- src/IDAStar.py
+++ src/IDAStar.py
@@ -1,31 +1,69 @@
-from node import Node
+import numpy as np
+
+from Node import Node
+from heuristics import manhattan_distance
+
 
 class IDAStar:
 
     def __init__(self, heuristic):
         self.heuristic = heuristic
+        self.goal_board = None
+        self.FOUND = object()
+        self.INFINITY = 9223372036854775807
+        self.path = []
 
-    def solve(self, start_board):
-        start_node = Node(start_board, 0, self.heuristic)
+    def solve(self, start_board, goal_board):
+        self.goal_board = goal_board
+        start_node = Node(start_board, 0, self.heuristic(start_board, goal_board))
         threshold = start_node.h
+
+        self.path.append(start_node)
+
         while True:
             temp = self._search(start_node, threshold)
-            if temp == self.goal:
-                return temp
-            if temp == iksdflk;sda
-                return
+            if temp == self.FOUND:
+                return self.path
+            if temp == self.INFINITY:
+                return None
             threshold = temp
 
-
     def _search(self, node, threshold):
-        if node.f > threshold or node == self.goal:
-            return node
-        minimum_cost = 9223372036854775807
-        for child in node.children:
+        if node.f > threshold:
+            return node.f
+        if node == self.goal_board:
+            return self.FOUND
+        minimum_cost = self.INFINITY
+        for child in node.children(self.heuristic, self.goal_board):
+            self.path.append(child)
             child_ida = self._search(child, threshold)
-            if child_ida == self.goal:
-                return child_ida
-            if child_ida.f < minimum_cost:
-                minimum_cost = child_ida.f
-        node.f
-        return node
+            if child_ida == self.FOUND:
+                return self.FOUND
+            self.path.pop()
+            if child_ida < minimum_cost:
+                minimum_cost = child_ida
+        return minimum_cost
+
+
+board = np.array([
+    [3, 2, 6],
+    [1, 4, 0],
+    [8, 7, 5],
+])
+
+# board = np.array([
+#     [1, 3, 4],
+#     [7, 0, 5],
+#     [6, 8, 2],
+# ])
+
+goal = np.array([
+    [1, 2, 3],
+    [8, 0, 4],
+    [7, 6, 5],
+])
+
+ida = IDAStar(manhattan_distance)
+path = ida.solve(board, goal)
+for node in path:
+    print(node)
--- src/heuristics.py
+++ src/heuristics.py
@@ -2,22 +2,19 @@ import numpy as np
 
 
 def manhattan_distance(board, goal):
-    board_flat = board.ravel()
-    goal_flat = goal.ravel()
-
     mandistance = 0
-    for element in board_flat:
-        distance = abs(np.where(goal_flat == element)[0][0] - np.where(board_flat == element)[0][0])
-        x, y = distance // board.shape[0], distance % board.shape[0]
-        mandistance += x + y
+    for element in board.ravel():
+        b_index = np.where(board == element)
+        g_index = np.where(goal == element)
+        mandistance += abs(g_index[0][0] - b_index[0][0]) + abs(g_index[1][0] - b_index[1][0])
     return mandistance
 
 
-board = np.array([
-    [1, 2, 5],
-    [3, 0, 6],
-    [7, 4, 8],
-])
+# board = np.array([
+#     [1, 2, 5],
+#     [3, 0, 6],
+#     [7, 4, 8],
+# ])
 
 # board = np.array([
 #     [1, 4, 3],
@@ -25,10 +22,10 @@ board = np.array([
 #     [7, 6, 5],
 # ])
 
-goal = np.array([
-    [1, 2, 3],
-    [8, 0, 4],
-    [7, 6, 5],
-])
+# goal = np.array([
+#     [1, 2, 3],
+#     [8, 0, 4],
+#     [7, 6, 5],
+# ])
 
-print(manhattan_distance(board, goal))
+# print(manhattan_distance(board, goal))
